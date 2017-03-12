f30affcb7e16c456dcd7eb8b91a9a4f5d0588b37
Katarzyna Etryk <kasiaetryk@gmail.com>
2017-03-12 17:42:33 +0100

    upload

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ $('#search').on('click', function(mEvent) {
             var mTitle = document.createElement('h3');
             var mPlot = document.createElement('p');
             var mPoster = document.createElement('span');
-            var mYear = document.createElement('span');
-            var mDirector = document.createElement('span');
-            var mGenre = document.createElement('span');
-            var mActor = document.createElement('span');
+            var mYear = document.createElement('p');
+            var mDirector = document.createElement('p');
+            var mGenre = document.createElement('p');
+            var mActor = document.createElement('p');
 
 
             mDiv.className = 'searchResult searchResult' + i;
@@ -42,26 +42,27 @@ $('#search').on('click', function(mEvent) {
             mActor.className = 'actor actor' + i;
 
             document.getElementById('container').appendChild(mDiv);
-            document.getElementById('searchResult' + i).appendChild(mTitle);
-            document.getElementById('searchResult' + i).appendChild(mPlot);
+
             document.getElementById('searchResult' + i).appendChild(mPoster);
+            document.getElementById('searchResult' + i).appendChild(mTitle);
             document.getElementById('searchResult' + i).appendChild(mYear);
-            document.getElementById('searchResult' + i).appendChild(mDirector);
             document.getElementById('searchResult' + i).appendChild(mGenre);
+            document.getElementById('searchResult' + i).appendChild(mDirector);
             document.getElementById('searchResult' + i).appendChild(mActor);
+            document.getElementById('searchResult' + i).appendChild(mPlot);
             
             var detailedURL = urlBase + '&i=' + mTable[i].imdbID;
             $.ajax(detailedURL, { 
               indexValue: i,
               complete: function(data){
                 var detailedData = $.parseJSON(data.responseText);
-                $('#searchResult' + this.indexValue).find('.title' + this.indexValue).text(detailedData.Title);
                 $('#searchResult' + this.indexValue).find('.poster' + this.indexValue).html('<img src="' + detailedData.Poster + '"/>');
-                $('#searchResult' + this.indexValue).find('.year' + this.indexValue).text(detailedData.Year);
-                $('#searchResult' + this.indexValue).find('.genre' + this.indexValue).text(detailedData.Genre);
-                $('#searchResult' + this.indexValue).find('.director' + this.indexValue).text(detailedData.Director);
-                $('#searchResult' + this.indexValue).find('.actor' + this.indexValue).text(detailedData.Actors);
-                $('#searchResult' + this.indexValue).find('.plot' + this.indexValue).text(detailedData.Plot); 
+                $('#searchResult' + this.indexValue).find('.title' + this.indexValue).text(detailedData.Title);
+                $('#searchResult' + this.indexValue).find('.year' + this.indexValue).text('Year: ' + detailedData.Year + ' ');
+                $('#searchResult' + this.indexValue).find('.genre' + this.indexValue).text('Genre: ' + detailedData.Genre + ' ');
+                $('#searchResult' + this.indexValue).find('.director' + this.indexValue).text('Director: ' + detailedData.Director + ' ');
+                $('#searchResult' + this.indexValue).find('.actor' + this.indexValue).text('Actors: ' + detailedData.Actors + ' ');
+                $('#searchResult' + this.indexValue).find('.plot' + this.indexValue).text('Plot: ' + detailedData.Plot + ' '); 
               }
             });
           }
